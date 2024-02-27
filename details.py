db3e7c9e593c84a96acef214583d04ec18b3d8b7
SimoneFerra <116488757+SimoneFerra@users.noreply.github.com>
2024-02-27 17:12:42 +0000

    Update details.py

--- details.py
+++ details.py
@@ -1,10 +1,16 @@
+#---GET DETAILS---
 
+# Request the user name
 name = input("Enter your name: ")
 
+# Request the user age
 age = input("Enter your age: ")
 
+# Request the user house number
 house_number = input("Enter your house number: ")
 
+# Request the user street name
 street_name = input("Enter your street_name: ")
 
+# Print out all the details
 print ("This is " + name + ". He is " + age + " years old and lives at house number " + house_number + " on " + street_name + ".")
